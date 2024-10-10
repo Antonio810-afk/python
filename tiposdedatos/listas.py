16d4bd74865da0bcff00ed4044ee7f73dc30924f
Antonio810-afk <antoniojoseromeroparada810@gmail.com>
2024-10-10 11:09:52 -0500

    listas

---
+++ tiposdedatos/listas.py
@@ -0,0 +1,8 @@
+lista=["w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,q"]
+#print(lista)
+#elemento=input("ingrese el valor a agregar a la lista")
+#lista.remove(float(elemento))
+print(lista)
+#lista.extend([10, 11, 12, 13])#sirve para agregar nuevos elementos a una lista existente
+lista.sort(reverse=True)
+print(lista)
